131601944328280e67db0bc05ed0f8eafc8c3298
AckermanNan <1178809645@qq.com>
2022-09-27 21:59:37 +0800

    贪吃蛇项目搭建

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# 发生错误时终止
+set -e
+
+# 构建
+npm run build
+
+# 进入构建文件夹
+cd dist
+
+# 如果你要部署到自定义域名
+git init
+git add -A
+git commit -m 'github-pages创建'
+
+
+# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:AckermanNanke/esurient-snake.git master:gh-pages
+
+cd -
---
+++ src/main.ts
@@ -0,0 +1,57 @@
+import { esurientSnake } from "./esurientSnake";
+import './style.css';
+
+const snakeClass = new esurientSnake();
+
+/**
+ * 节流函数
+ * 防止一直点击蛇不会动
+ * 时间设为与蛇移动时间一致
+ */
+function throttle(func: Function, wait: number) {
+  let preTime = 0;
+  return function () {
+    let now = Date.now();
+    if (Date.now() - preTime > wait) {
+      func.apply(this, arguments);
+      preTime = now;
+    }
+  }
+}
+/**
+ * 监听键盘
+ */
+document.addEventListener("keydown", throttle((event: KeyboardEvent) => {
+  // 鼠标监听
+  // let currentX = event.offsetX;
+  // let currentY = event.offsetY;
+  // startSnake.snakeMove(currentX, currentY);
+  if (snakeClass.isPause) {
+    snakeClass.snakeMove(snakeClass.direction!);
+  } else if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(event.code)) {
+    snakeClass.snakeMove(event.code as "ArrowUp" | "ArrowDown" | "ArrowLeft" | "ArrowRight");
+  }
+}, 200))
+/**
+ * 开始
+ */
+document.getElementById("start")!.addEventListener("click", throttle(() => {
+  if (snakeClass.isPause) {
+    snakeClass.snakeMove(snakeClass.direction!);
+  } else {
+    snakeClass.snakeMove("ArrowRight");
+  }
+}, 200))
+/**
+ * 暂停
+ */
+document.getElementById("pause")!.addEventListener("click", throttle(() => {
+  clearInterval(Number(snakeClass.Timer));
+  snakeClass.isPause = true
+}, 200))
+/**
+ * 停止
+ */
+document.getElementById("stop")!.addEventListener("click", throttle(() => {
+  snakeClass.reseteSurientSnake();
+}, 200)) 
