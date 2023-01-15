f50202fd2dd580d902d36cb81f72187916e29454
AckermanNan <1178809645@qq.com>
2023-01-15 22:31:27 +0800

    测试

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command, mode }) => {
+  return {
+    server: {
+      port: 8998
+    },
+    base: command === "build" ? "/esurient-snake/" : "/",
+  }
 })
